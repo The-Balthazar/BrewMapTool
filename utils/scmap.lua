64afa138e2d977bc5e6f90ff70babb7a16514f44
Balthazar <the.balthazar@Gmail.com>
2024-07-10 10:50:53 +0100

    `scmapUtils.writeDatastream`

--- utils/scmap.lua
+++ utils/scmap.lua
@@ -268,6 +268,246 @@ function scmapUtils.readDatastream(scmapData)
     return data
 end
 
+local function hex2bin(hex) return string.char(tonumber(hex, 16)) end
+local function hex2bin2(a,b) return hex2bin(a)..hex2bin(b) end
+local function hex2bin4(a,b,c,d) return hex2bin(a)..hex2bin(b)..hex2bin(c)..hex2bin(d) end
+local function hexSplit4(val) return hex2bin4(val:sub(-8,-7),val:sub(-6,-5),val:sub(-4,-3),val:sub(-2,-1)) end
+local function hexSplitFlip4(val) return hex2bin4(val:sub(-2,-1),val:sub(-4,-3),val:sub(-6,-5),val:sub(-8,-7)) end
+
+function scmapUtils.writeDatastream(files, filename)
+    local fileData = scmapDef.header[1]..scmapDef.header[2]..scmapDef.header[3]..scmapDef.header[2]
+    local data = files['data.lua']()
+
+    print(filename, "starting packing")
+
+    local function float(val) fileData = fileData..hexSplit4(val) end
+    local function vec2(vec) float(vec[1]) float(vec[2]) end
+    local function vec3(vec) float(vec[1]) float(vec[2]) float(vec[3]) end
+    local function vec4(vec) float(vec[1]) float(vec[2]) float(vec[3]) float(vec[4]) end
+    local function int(val)
+        if type(val)=='string' then
+            fileData = fileData..hexSplit4(val)
+        elseif val==-1 then
+            fileData = fileData..'\255\255\255\255'
+        elseif type(val)=='number' then
+            fileData = fileData..hexSplitFlip4(('%0.8x'):format(val))
+        end
+    end
+    local function image(img)
+        int(img:len())
+        fileData = fileData..img
+    end
+    local function stringNull(str) fileData = fileData..(str or '')..'\000' end
+
+    float(data.floatWidth)
+    float(data.floatHeight)
+
+    fileData = fileData..'\000\000\000\000\000\000'
+
+    if files['previewImage.dds']:len()~=262272 then print"Warning preview image isn't the expected 262272 bytes" end
+    image(files['previewImage.dds'])
+
+    int(data.version)
+
+    int(data.size[1])
+    int(data.size[2])
+    float(data.heightmapScale)
+    local expectedHeightmapSize = (data.size[1]+1)*(data.size[2]+1)*2
+    if #files['heightmap.raw']~=expectedHeightmapSize then print("Warning: Heightmap", #files['heightmap.raw'], "bytes. expected: ", expectedHeightmapSize, "bytes") end
+    fileData = fileData..files['heightmap.raw']..'\000'
+
+    stringNull(data.shaderPath)
+    stringNull(data.backgroundPath)
+    stringNull(data.skyCubePath)
+
+    int(#data.cubeMaps)
+    for i, map in ipairs(data.cubeMaps) do
+        stringNull(map.name)
+        stringNull(map.path)
+    end
+
+    local l = data.lightingSettings
+    float(l.lightingMultiplier)
+    vec3(l.sunDirection)
+    vec3(l.sunAmbience)
+    vec3(l.sunColor)
+    vec3(l.shadowFillColor)
+    vec4(l.specularColor)
+    float(l.bloom)
+    vec3(l.fogColor)
+    float(l.fogStart)
+    float(l.fogEnd)
+
+    l = data.waterSettings
+    fileData = fileData..string.char(l.waterPresent and 1 or 0)
+    float(l.elevation)
+    float(l.elevationDeep)
+    float(l.elevationAbyss)
+    vec3(l.surfaceColor)
+    vec2(l.colorLerp)
+    float(l.refractionScale)
+    float(l.fresnelBias)
+    float(l.fresnelPower)
+    float(l.unitReflection)
+    float(l.skyReflection)
+    float(l.sunShininess)
+    float(l.sunStrength)
+    vec3(l.sunDirection)
+    vec3(l.sunColor)
+    float(l.sunReflection)
+    float(l.sunGlow)
+    stringNull(l.texPathCubeMap)
+    stringNull(l.texPathWaterRamp)
+    vec4(l.waveNormalRepeats)
+    if #l.waveTextures~=4 then print"Warning: waveTextures an unexpected array length: expected 4" end
+    for i, v in ipairs(l.waveTextures) do
+        vec2(v.movement)
+        stringNull(v.path)
+    end
+
+    int(#l.waveGenerators)
+    for i, v in ipairs(l.waveGenerators) do
+        stringNull(v.textureName)
+        stringNull(v.rampName)
+        vec3(v.position)
+        float(v.rotation)
+        vec3(v.velocity)
+
+        float(v.lifeTimeFirst)
+        float(v.lifeTimeSecond)
+        float(v.periodFirst)
+        float(v.periodSecond)
+        float(v.scaleFirst)
+        float(v.scaleSecond)
+        float(v.frameCount)
+        float(v.frameRateFirst)
+        float(v.frameRateSecond)
+        float(v.stripCount)
+    end
+
+    int(data.miniMapContourInterval)
+    fileData = fileData..hexSplit4(data.miniMapDeepWaterColor)
+    fileData = fileData..hexSplit4(data.miniMapContourColor)
+    fileData = fileData..hexSplit4(data.miniMapShoreColor)
+    fileData = fileData..hexSplit4(data.miniMapLandStartColor)
+    fileData = fileData..hexSplit4(data.miniMapLandEndColor)
+
+    if data.version>56 then
+        fileData = fileData..hexSplit4(data.unknownFA)
+    end
+
+    if #data.textures~=10 then print"Warning: textures an unexpected array length: expected 10" end
+    if #data.normals~=9 then print"Warning: normals an unexpected array length: expected 9" end
+    for i, v in ipairs(data.textures) do stringNull(v.path) float(v.scale) end
+    for i, v in ipairs(data.normals) do stringNull(v.path) float(v.scale) end
+
+    fileData = fileData..hexSplit4(data.unknown1)
+    fileData = fileData..hexSplit4(data.unknown2)
+
+    int(#data.decals)
+    for i, decal in ipairs(data.decals) do
+        int(decal.id)
+        int(decal.type)
+        int(#decal.textures)
+        for i, path in ipairs(decal.textures) do
+            int(#path)
+            fileData = fileData..path
+        end
+        vec3(decal.scale)
+        vec3(decal.position)
+        vec3(decal.rotation)
+        float(decal.LODCutoff)
+        float(decal.LODCutoffMin)
+        int(decal.army)
+    end
+
+    int(#data.decalGroups)
+    for i, group in ipairs(data.decalGroups) do
+        int(group.id)
+        stringNull(group.name)
+        int(#group.data)
+        for i, v in ipairs(group.data) do
+            int(v)
+        end
+    end
+
+    int(data.intWidth)
+    int(data.intHeight)
+
+    int(1)
+
+    image(files['normalMap.dds'])
+    image(files['textureMaskLow.dds'])
+    image(files['textureMaskHigh.dds'])
+
+    int(1)
+
+    image(files['waterMap.dds'])
+
+    fileData = fileData
+        ..files['waterFoamMask.raw']
+        ..files['waterFlatness.raw']
+        ..files['waterDepthBiasMask.raw']
+        ..files['terrainType.raw']
+
+    if data.version>=60 then
+        l = data.skyBox
+        vec3(l.position)
+        float(l.horizonHeight)
+        float(l.scale)
+        float(l.subHeight)
+        int(l.subDivAx)
+        int(l.subDivHeight)
+        float(l.zenithHeight)
+        vec3(l.horizonColor)
+        vec3(l.zenithColor)
+        float(l.decalGlowMultiplier)
+
+        stringNull(l.albedo)
+        stringNull(l.glow)
+
+        int(#l.planets)
+        for i, planet in ipairs(l.planets) do
+            vec3(planet.position)
+            float(planet.rotation)
+            vec2(planet.scale)
+            vec4(planet.uv)
+        end
+        fileData = fileData
+            ..string.char(l.midColor[1])
+            ..string.char(l.midColor[2])
+            ..string.char(l.midColor[3])
+
+        float(l.cirrusMultiplier)
+        vec3(l.cirrusColor)
+        stringNull(l.cirrusTexture)
+
+        int(#l.cirrusLayers)
+        for i, layer in ipairs(l.cirrusLayers) do
+            vec2(layer.frequency)
+            float(layer.speed)
+            vec2(layer.direction)
+        end
+
+        float(l.clouds7)
+    end
+
+    int(#data.props)
+    for i, prop in ipairs(data.props) do
+        stringNull(prop.path)
+        vec3(prop.position)
+        vec3(prop.rotationX)
+        vec3(prop.rotationY)
+        vec3(prop.rotationZ)
+        vec3(prop.scale)
+    end
+    print("Writing", filename)
+    love.filesystem.createDirectory('packed')
+    local done, msg = love.filesystem.write('packed/'..filename, fileData)
+    print(done and "Write complete" or msg)
+    love.system.openURL(love.filesystem.getSaveDirectory()..'/packed')
+end
+
 function scmapUtils.readHeightmap(heightmapRaw, width, height, heightmapScale)
     local min, max = math.huge, 0
 
