9cfbd808039f528794bfd23fd215007aa2286faf
Balthazar <the.balthazar@Gmail.com>
2024-07-18 18:22:55 +0100

    removed the cast step from value serialization.

--- utils/maths.lua
+++ utils/maths.lua
@@ -24,7 +24,7 @@ function math.IMBUInt(bytes)  return fficast('uint32_t*', bytes)[0] end
 function math.IMBInt(bytes)   return fficast('int32_t*',  bytes)[0] end
 function math.IMBFloat(bytes) return fficast('float*',    bytes)[0] end
 
-function math.shortToIBM(val) return ffistring(fficast('uint8_t(*)[4]', ffinew("uint16_t[1]", {val}))[0], 4) end
-function math.uIntToIBM(val)  return ffistring(fficast('uint8_t(*)[4]', ffinew("uint32_t[1]", {val}))[0], 4) end
-function math.intToIBM(val)   return ffistring(fficast('uint8_t(*)[4]', ffinew("int32_t[1]",  {val}))[0], 4) end
-function math.floatToIBM(val) return ffistring(fficast('uint8_t(*)[4]', ffinew("float[1]",    {val}))[0], 4) end
+function math.shortToIBM(val) return ffistring(ffinew("uint16_t[1]", {val}), 4) end
+function math.uIntToIBM(val)  return ffistring(ffinew("uint32_t[1]", {val}), 4) end
+function math.intToIBM(val)   return ffistring(ffinew("int32_t[1]",  {val}), 4) end
+function math.floatToIBM(val) return ffistring(ffinew("float[1]",    {val}), 4) end
